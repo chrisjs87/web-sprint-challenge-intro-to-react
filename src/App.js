357e97e2240fc094c676369abfa8f62e0c39c5a6
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-14 12:56:04 -0500

    Set up the pulling of data from the api and prepped state for use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-const App = () => {
+export default function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +10,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [charactersData, setCharactersData] = useState([]);
+  
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(function (response) {
+      setCharactersData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
     </div>
   );
 }
-
-export default App;
